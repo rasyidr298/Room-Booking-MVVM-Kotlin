aef24a4e4c568be1c0d2b4e29245d679d5703015
rasyidr298 <rasyidridla298@gmail.com>
2020-04-07 22:50:28 +0700

    membuat Room Dtabase untuk menyimpan Sesion Login User

---
+++ app/src/main/java/com/rrdev/mvvmkotlin/data/db/UserDao.kt
@@ -0,0 +1,18 @@
+package com.rrdev.mvvmtrial.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.rrdev.mvvmtrial.data.db.entities.CURRENT_USER_ID
+import com.rrdev.mvvmtrial.data.db.entities.User
+
+@Dao
+interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(user: User) : Long
+
+    @Query("SELECT * FROM user WHERE uid = $CURRENT_USER_ID")
+    fun getUser() : LiveData<User>
+}
